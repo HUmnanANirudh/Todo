33a90a07ca3cf35b65ad028a8aa134d128ab85d5
HUmnanANirudh <tanirudhganesh@gmail.com>
2025-04-11 23:28:06 +0530

    Final Touches

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,17 @@ const PORT = 6969;
 app.use("/api/v1/todo",Todos)
 
 app.get("/",async(req:Request,res:Response)=>{
-    res.send("Hello world")
+    res.send(`
+    <h1>Welcome</h1>
+    <p>Here are the available endpoints:</p>
+    <ul>
+      <li>GET /api/v1/todo/bulk - Get all todos</li>
+      <li>GET /api/v1/todo/:id - Get a specific todo by ID</li>
+      <li>POST /api/v1/todo - Create a new todo</li>
+      <li>PUT /api/v1/todo/:id - Update specific todo by ID</li>
+      <li>DELETE /api/v1/todo/:id - Delete a specific todo by ID</li>
+    </ul>
+  `)
 })
 
 app.listen(PORT , ()=>console.log(`running http://localhost:${PORT}`))
